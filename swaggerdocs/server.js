25341348a39cf4c5171e5eb345cbd7d1e9784b6b
Rupam Shil <rupamshil111@gmail.com>
2021-11-21 18:47:37 +0530

    swaggers params added

--- swaggerdocs/server.js
+++ swaggerdocs/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const res = require('express/lib/response');
 
 const app = express();
+app.use(express.json());
 
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
@@ -34,6 +35,21 @@ app.get('/', (req, res) => {
 
 app.get('/api/v1/rupam', (req, res) => {
 	res.send("Hello from Rupam's end!!");
+});
+
+app.get('/api/v1/rupamobject', (req, res) => {
+	res.json({
+		id: '21',
+		name: 'learn vuejs',
+		price: 399,
+	});
+});
+app.get('/api/v1/courses', (req, res) => {
+	res.json(courses);
+});
+app.get('/api/v1/mycourse/:courseId', (req, res) => {
+	const myCourse = courses.find((course) => course.id === req.params.courseId);
+	myCourse ? res.json(myCourse) : res.json({ err: 'not found' });
 });
 
 app.listen(4000, () => console.log('Server running on port 4000'));
